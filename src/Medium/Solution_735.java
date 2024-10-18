44ea23eac92974d931aac7f57238ed4f86deaec1
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-10-18 19:01:05 +0300

    simpler implmentation

--- src/Medium/Solution_735.java
+++ src/Medium/Solution_735.java
@@ -49,37 +49,21 @@ public class Solution_735 {
     }*/
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> stack = new Stack<>();
-        stack.push(asteroids[0]);
-        for(int i = 1; i < asteroids.length;i++){
-            if(stack.isEmpty()){
-                stack.push(asteroids[i]);
-                continue;
+        for(int a : asteroids){
+            if(a > 0){
+                stack.push(a);
             }
-            int tmp = stack.peek();
-            if((tmp < 0 && asteroids[i] < 0) || (tmp > 0 && asteroids[i] > 0) || (tmp < 0 && asteroids[i] > 0)){
-                stack.push(asteroids[i]);
-            }
-            else {
-                tmp = Math.abs(tmp);
-                int idx = Math.abs(asteroids[i]);
-                if(tmp == idx){
+            else{
+                while (!stack.isEmpty() && stack.peek() > 0 && stack.peek() < -a){
                     stack.pop();
                 }
-                else if (tmp < idx){
-                    stack.pop();
-                    if(!stack.isEmpty()){
-                        tmp = stack.peek();
-                        if((tmp < 0 && asteroids[i] < 0) || (tmp > 0 && asteroids[i] > 0 )){
-                            stack.push(asteroids[i]);
-                        }
-                        else {
-                            i--;
-                        }
-                    }
-                    else {
-                        stack.push(asteroids[i]);
-                    }
+
+                if(stack.isEmpty() || stack.peek() < 0){
+                    stack.push(a);
                 }
+
+                if(stack.peek() == -a)
+                    stack.pop();
             }
         }
         int[] result = new int[stack.size()];
