0001400388211c26c026339d3d7df156afa6dae9
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-10-27 14:20:32 +0300

    1161 MEDIUM BFS QUEUE another approach

--- src/Medium/Solution_1161.java
+++ src/Medium/Solution_1161.java
@@ -1,9 +1,12 @@
 package Medium;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
 public class Solution_1161 {
+    /*
     Map<Integer, Integer> lvlSums = new HashMap<>();
     public int maxLevelSum(TreeNode root) {
         helper(root, 0);
@@ -26,6 +29,39 @@ public class Solution_1161 {
         lvlSums.put(lvl, lvlSums.getOrDefault(lvl,0)+node.val);
         helper(node.left, lvl+1);
         helper(node.right, lvl+1);
+    }
+    */
+
+    public int maxLevelSum(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+
+        int maxSum=Integer.MIN_VALUE,maxLevel=0,level=0;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            int sum=0;
+            int n=queue.size();
+            level++;
+            for(int i=0;i<n;i++){
+                TreeNode currNode=queue.poll();
+                sum+=currNode.val;
+
+                if(currNode.left!=null){
+                    queue.add(currNode.left);
+                }
+                if(currNode.right!=null){
+                    queue.add(currNode.right);
+                }
+            }
+            if(sum>maxSum){
+                maxSum=sum;
+                maxLevel=level;
+            }
+        }
+        return maxLevel;
     }
 
 }
