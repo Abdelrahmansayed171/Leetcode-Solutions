7c480804011d743cd357b0d7fff9ae446211ca21
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-10-30 19:09:30 +0300

    215 FIRST HEAP/Priority queue SOLVED BY MEAN HEAP APPROACH

--- src/Medium/Solution_215.java
+++ src/Medium/Solution_215.java
@@ -5,7 +5,7 @@ import java.util.PriorityQueue;
 public class Solution_215 {
 
     // QUICK-SELECT APPROACH AVG(O(n)), WORST(O(n*n))
-    public int findKthLargest(int[] nums, int k) {
+/*    public int findKthLargest(int[] nums, int k) {
         // define start, end indexes
         int left = 0, right = nums.length-1;
         // Do our quick sort until we get element stored at nums.size - k index
@@ -37,8 +37,22 @@ public class Solution_215 {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
-    }
+    }*/
 
     // MAX HEAP (Priority QUEUE) APPROACH O(n*log(n))
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            minHeap.offer(nums[i]);
+        }
 
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.offer(nums[i]);
+            }
+        }
+
+        return minHeap.peek();
+    }
 }
