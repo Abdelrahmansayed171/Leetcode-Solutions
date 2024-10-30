e65d9f75368bd52c0c914af2f57fa6986a5fed57
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-10-30 19:09:02 +0300

    215 FIRST HEAP/Priority queue problem SOLVED BY QUICK SELECT ALGORITHM

--- src/Medium/Solution_215.java
+++ src/Medium/Solution_215.java
@@ -1,14 +1,36 @@
 package Medium;
 
+import java.util.PriorityQueue;
+
 public class Solution_215 {
 
     // QUICK-SELECT APPROACH AVG(O(n)), WORST(O(n*n))
     public int findKthLargest(int[] nums, int k) {
+        // define start, end indexes
         int left = 0, right = nums.length-1;
+        // Do our quick sort until we get element stored at nums.size - k index
         while(true){
             int pivot_index = (left+right)/2;
-
+            int sorted_position = quickSelect(nums, left, right, pivot_index);
+            if(nums.length - sorted_position == k)
+                return nums[sorted_position];
+            else if(nums.length - sorted_position > k)
+                left = sorted_position + 1;
+            else
+                right = sorted_position -1;
         }
+    }
+    private int quickSelect(int[] nums, int left, int right, int pivot){
+        swap(nums, pivot, right);
+        int j = left;
+        for(int i = left; i < right; i++){
+            if(nums[i] < nums[right]){
+                swap(nums, i,j);
+                j++;
+            }
+        }
+        swap(nums,right, j);
+        return j;
     }
 
     private void swap(int[] nums, int i, int j) {
@@ -17,6 +39,6 @@ public class Solution_215 {
         nums[j] = temp;
     }
 
-    // MAX HEAP APPROACH O(n*log(n))
+    // MAX HEAP (Priority QUEUE) APPROACH O(n*log(n))
 
 }
