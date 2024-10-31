57570b2467336a3fe50d458fc089307447dc548e
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-10-31 16:47:28 +0300

    2336 priorty Q, Heap problem Solved

--- src/Medium/SmallestInfiniteSet.java
+++ src/Medium/SmallestInfiniteSet.java
@@ -1,16 +1,32 @@
 package Medium;
 
-public class SmallestInfiniteSet {
-    public SmallestInfiniteSet() {
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
 
+public class SmallestInfiniteSet {
+    PriorityQueue<Integer> heap;
+    Set<Integer> hashTable;
+    public SmallestInfiniteSet() {
+        heap = new PriorityQueue<>();
+        hashTable = new HashSet<>();
+        for(int i =1; i <= 1000;i++){
+            heap.add(i);
+            hashTable.add(i);
+        }
     }
 
     public int popSmallest() {
-
+        int smallest = heap.poll();
+        hashTable.remove(smallest);
+        return smallest;
     }
 
     public void addBack(int num) {
-
+        if(hashTable.contains(num))
+            return;
+        heap.offer(num);
+        hashTable.add(num);
     }
 }
 
