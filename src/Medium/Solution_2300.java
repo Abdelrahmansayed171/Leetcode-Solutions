3d7454478222afa89465da5d6310aec2087876b8
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-02 12:40:48 +0200

    2300 Binary Search 2nd problem attempt

--- src/Medium/Solution_2300.java
+++ src/Medium/Solution_2300.java
@@ -1,4 +1,28 @@
 package Medium;
 
+import java.util.Arrays;
+
 public class Solution_2300 {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        Arrays.sort(potions); // m * log(m)
+        int n = spells.length;
+        int[] res = new int [n];
+        for(int i = 0; i < n;i++){
+            int left = 0, right = spells.length-1;
+            while(left <= right){
+                int mid = left + (right-left)/2;
+                if ((long) spells[i] * potions[mid] < success){
+                    if((long) spells[i] * potions[mid+1] >= success){
+                        res[i] = potions.length - mid+1;
+                        break;
+                    }
+                    left = mid+1;
+                }
+                else{
+                    right = mid-1;
+                }
+            }
+        }
+        return res;
+    }
 }
