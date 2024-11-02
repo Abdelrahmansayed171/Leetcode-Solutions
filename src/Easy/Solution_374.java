df1bb666003141181ef227e9786900aff852315a
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-02 11:15:58 +0200

    handling class error

--- src/Easy/Solution_374.java
+++ src/Easy/Solution_374.java
@@ -1,20 +1,19 @@
 package Easy;
 
 public class Solution_374 {
-    public class Solution extends GuessGame {
-        public int guessNumber(int n) {
-            int start = 0;
-            int end = n;
-            while(start<=end){
-                int mid = start + (end - start)/2;
-                int num = guess(mid);
-                if(num == 0)
-                    return mid;
-                else if(num == -1)
-                    end = mid-1;
-                else
-                    start = mid+1;
-            }
-            return start;
+    /*public int guessNumber(int n) {
+        int start = 0;
+        int end = n;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int num = guess(mid);
+            if (num == 0)
+                return mid;
+            else if (num == -1)
+                end = mid - 1;
+            else
+                start = mid + 1;
         }
+        return start;
+    }*/
 }
