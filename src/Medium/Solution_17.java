c4941dce5fcc2d0e60d80c5cb9792c96c7b034d4
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-04 16:12:38 +0200

    17 Backtracking first problem solved in 0ms and 5ms

--- src/Medium/Solution_17.java
+++ src/Medium/Solution_17.java
@@ -1,24 +1,27 @@
 package Medium;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Solution_17 {
-    String[] digitStrings = new String[10];
-    public Solution_17(){
-        char next = 'a';
-        for(int i = 2;i < 10;i++){
-            digitStrings[i] = "";
-            digitStrings[i] += next++;
-            digitStrings[i] += next++;
-            digitStrings[i] += next++;
-            digitStrings[i] += i == 7 || i == 9? next++:"";
-        }
-    }
-    List<String> res = new ArrayList<>();
+    /*List<String> res = new ArrayList<>();
     public List<String> letterCombinations(String digits) {
         if(digits.isEmpty())
             return new ArrayList<>();
+        String[] digitStrings = {
+                "",     // 0 (no letters for 0)
+                "",     // 1 (no letters for 1)
+                "abc",  // 2
+                "def",  // 3
+                "ghi",  // 4
+                "jkl",  // 5
+                "mno",  // 6
+                "pqrs", // 7
+                "tuv",  // 8
+                "wxyz"  // 9
+        };
         List<String> strings = new ArrayList<>();
         for(char c : digits.toCharArray()){
             System.out.println(c-'0');
@@ -35,5 +38,41 @@ public class Solution_17 {
         }
         for(int i = 0; i < strings.get(idx).length() ;i++)
             backtrack(strings, idx+1, s + strings.get(idx).charAt(i));
-    }
+    }*/
+        public List<String> letterCombinations(String digits) {
+            List<String> res = new ArrayList<>();
+
+            if (digits == null || digits.length() == 0) {
+                return res;
+            }
+
+            Map<Character, String> digitToLetters = new HashMap<>();
+            digitToLetters.put('2', "abc");
+            digitToLetters.put('3', "def");
+            digitToLetters.put('4', "ghi");
+            digitToLetters.put('5', "jkl");
+            digitToLetters.put('6', "mno");
+            digitToLetters.put('7', "pqrs");
+            digitToLetters.put('8', "tuv");
+            digitToLetters.put('9', "wxyz");
+
+            backtrack(digits, 0, new StringBuilder(), res, digitToLetters);
+
+            return res;
+        }
+
+        private void backtrack(String digits, int idx, StringBuilder comb, List<String> res, Map<Character, String> digitToLetters) {
+            if (idx == digits.length()) {
+                res.add(comb.toString());
+                return;
+            }
+
+            String letters = digitToLetters.get(digits.charAt(idx));
+            for (char letter : letters.toCharArray()) {
+                comb.append(letter);
+                backtrack(digits, idx + 1, comb, res, digitToLetters);
+                comb.deleteCharAt(comb.length() - 1);
+            }
+        }
+
 }
