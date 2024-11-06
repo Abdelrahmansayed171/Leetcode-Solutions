a38175c81d71a210729e8c03448a71eca0b023db
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-06 10:31:24 +0200

    1137 first 1D DP Easy O(3^n) to O(n) solved

--- src/Easy/Solution_1137.java
+++ src/Easy/Solution_1137.java
@@ -2,11 +2,24 @@ package Easy;
 
 public class Solution_1137 {
     // Recursion Solution
-    public int tribonacci(int n) {
+    /*public int tribonacci(int n) {
         if(n == 0 || n == 1)
             return n;
         if(n == 2)
             return 1;
         return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+    }*/
+
+    public int tribonacci(int n) {
+        int[] arr = {0 , 1 ,1};
+        if( n < 3 )
+            return arr[n];
+        for(int i = 3;i <= n;i++){
+            int sum = arr[0] + arr[1] + arr[2];
+            arr[0] = arr[1];
+            arr[1] = arr[2];
+            arr[2] = sum;
+        }
+        return arr[2];
     }
 }
