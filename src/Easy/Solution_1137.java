c319f63ec6f75e02528da98f4dc8be54f6f79bc1
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-06 09:57:32 +0200

    1137 first 1D DP Easy Recursion TLE Solution

--- src/Easy/Solution_1137.java
+++ src/Easy/Solution_1137.java
@@ -1,7 +1,12 @@
 package Easy;
 
 public class Solution_1137 {
+    // Recursion Solution
     public int tribonacci(int n) {
-
+        if(n == 0 || n == 1)
+            return n;
+        if(n == 2)
+            return 1;
+        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
     }
 }
