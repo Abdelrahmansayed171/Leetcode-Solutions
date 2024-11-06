2b2a957e9d4e92c5659adee3472bc294dba46337
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-06 14:03:58 +0200

    746 second 1D DP Easy Solved

--- src/Easy/Solution_746.java
+++ src/Easy/Solution_746.java
@@ -2,6 +2,9 @@ package Easy;
 
 public class Solution_746 {
     public int minCostClimbingStairs(int[] cost) {
-
+        for(int i = cost.length - 3; i >= 0;i--){
+            cost[i] += Math.min(cost[i+1], cost[i+2]);
+        }
+        return Math.min(cost[0], cost[1]);
     }
 }
