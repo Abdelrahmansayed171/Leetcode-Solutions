36287ce724d23a5cac56dc59143034d201051735
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-07 12:56:47 +0200

    ##5arra## 790 last 1D DP Medium Solved ## mathematically ##.

--- src/Medium/Solution_790.java
+++ src/Medium/Solution_790.java
@@ -2,6 +2,12 @@ package Medium;
 
 public class Solution_790 {
     public int numTilings(int n) {
-
+        long[] arr = new long[n+3];
+        int mod = (int) (1e9 + 7);
+        arr[1] = 1; arr[2] = 2; arr[3] = 5;
+        for(int i = 4; i<=n;i++){
+            arr[i] = (arr[i-1]*2 + arr[i-3]) % mod;
+        }
+        return (int) arr[n];
     }
 }
