203f5d9b2cdc35103b91083c1f8391a30f11e6a4
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-07 09:41:42 +0200

    198 third 1D DP Medium Solved another approach.

--- src/Medium/Solution_198.java
+++ src/Medium/Solution_198.java
@@ -1,7 +1,7 @@
 package Medium;
 
 public class Solution_198 {
-        public int rob(int[] nums) {
+        /*public int rob(int[] nums) {
             if(nums.length < 2)
                 return nums[0];
             nums[1] = Math.max(nums[0], nums[1]);
@@ -9,5 +9,16 @@ public class Solution_198 {
                 nums[i] = Math.max(nums[i] + nums[i-2], nums[i-1]);
             }
             return nums[nums.length-1];
+        }*/
+
+    public int rob(int[] nums){
+        int max1 = 0, max2 = 0;
+        for(int i = 0; i < nums.length;i++){
+            max1 = Math.max(max1 + nums[i], max2);
+            int tmp = max2;
+            max2 = max1;
+            max1 = tmp;
         }
+        return max2;
+    }
 }
