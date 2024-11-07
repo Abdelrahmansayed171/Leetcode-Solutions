4c8bc38f3319c4a9a6f504378811574a9ed3b48e
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-07 09:27:37 +0200

    198 third 1D DP Medium Solved successfully.

--- src/Medium/Solution_198.java
+++ src/Medium/Solution_198.java
@@ -1,7 +1,13 @@
 package Medium;
 
 public class Solution_198 {
-    public int rob(int[] nums) {
-        return 0;
-    }
+        public int rob(int[] nums) {
+            if(nums.length < 2)
+                return nums[0];
+            nums[1] = Math.max(nums[0], nums[1]);
+            for(int i = 2;i < nums.length;i++){
+                nums[i] = Math.max(nums[i] + nums[i-2], nums[i-1]);
+            }
+            return nums[nums.length-1];
+        }
 }
