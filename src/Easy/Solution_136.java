cfdf6463f0a1a2a695dffd8cde0729ca151104c3
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-11 11:51:08 +0200

    136 2nd easy bit manipulation XOR Solution.

--- src/Easy/Solution_136.java
+++ src/Easy/Solution_136.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution_136 {
-    public int singleNumber(int[] nums) {
+    /*public int singleNumber(int[] nums) {
         Map<Integer, Integer> occ = new HashMap<>();
         for (int num : nums)
             occ.put(num, occ.getOrDefault(num, 0) + 1);
@@ -13,5 +13,19 @@ public class Solution_136 {
                 return key;
         }
         return 0;
+    }*/
+
+    /*
+    * Conclusion1:
+    *   XOR doesn't obey to order
+    *   So, all duplicates will be = 0
+    *   4 = 100 ^ 000 = 100, so Result will contain the only unique value
+    * */
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for(int num : nums){
+            res ^= num;
+        }
+        return res;
     }
 }
