ff755873e2d1c315333854b4b0c45f084e5ea1db
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-11 18:09:59 +0200

    1268 last medium Trie Solved 2 pointers & Sorting.

--- src/Medium/Solution_1268.java
+++ src/Medium/Solution_1268.java
@@ -5,25 +5,20 @@ import java.util.*;
 public class Solution_1268 {
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
         Arrays.sort(products);
-        List<List<String>> result = new ArrayList<>();
-        Set<String> tmp = new HashSet<>();
-        for(int j = 0; j < searchWord.length() ;j++){
-            char c = searchWord.charAt(j);
-            List<String> list = new ArrayList<>();
-            for(int i = 0 ; i < products.length;i++){
-                if(j == 0 && c == products[i].charAt(j))
-                    tmp.add(products[i]);
-                if(j < products[i].length() && products[i].charAt(j) == c && tmp.contains(products[i]) )
-                    list.add(products[i]);
+        List<List<String>> ans = new ArrayList<>();
+        int left = 0, right = products.length - 1;
+        for (int i = 0; i < searchWord.length(); i++) {
+            char c = searchWord.charAt(i);
+            List<String> res = new ArrayList<>();
+
+            while (left <= right && (products[left].length() <= i || products[left].charAt(i) < c)) left++;
+            while (left <= right && (products[right].length() <= i || products[right].charAt(i) > c)) right--;
+
+            for (int j = 0; j < 3 && left + j <= right; j++) {
+                res.add(products[left + j]);
             }
-            if(list.size() > 3) {
-                List<String> sublist = new ArrayList<>();
-                sublist.add(list.get(0)); sublist.add(list.get(1)); sublist.add(list.get(2));
-                result.add(sublist);
-            }
-            else
-                result.add(list);
+            ans.add(res);
         }
-        return result;
+        return ans;
     }
 }
