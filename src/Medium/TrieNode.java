e70d9775488f5d7d8fb58f8be9c5413a96c64ed9
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-11 15:52:43 +0200

    208 First medium Trie Node created.

--- src/Medium/TrieNode.java
+++ src/Medium/TrieNode.java
@@ -1,4 +1,11 @@
 package Medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TrieNode {
+    TrieNode(){
+        Map<Character, TrieNode> children = new HashMap<>();
+        boolean fullWord = false;
+    }
 }
