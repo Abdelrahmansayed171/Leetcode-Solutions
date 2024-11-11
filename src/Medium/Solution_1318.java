bee0a30004c4a40caa2412982598a8981846e459
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-11 13:46:34 +0200

    1318 last medium bit manipulation Solved.

--- src/Medium/Solution_1318.java
+++ src/Medium/Solution_1318.java
@@ -2,6 +2,14 @@ package Medium;
 
 public class Solution_1318 {
     public int minFlips(int a, int b, int c) {
-
+        int res = 0;
+        while(a > 0 || b > 0 || c > 0){
+            if((c&1) == 1 && (a&1) == 0 && (b&1) == 0)
+                res++;
+            if((c&1) == 0)
+                res += (a&1) + (b&1);
+            a>>=1 ; b>>=1; c>>=1;
+        }
+        return res;
     }
 }
