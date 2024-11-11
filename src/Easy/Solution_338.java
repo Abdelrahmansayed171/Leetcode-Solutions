f8bf74bfa46826f44a631bbdb2c313175901577d
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-11 10:54:36 +0200

    delete redundant line of code.

--- src/Easy/Solution_338.java
+++ src/Easy/Solution_338.java
@@ -18,7 +18,6 @@ public class Solution_338 {
         * */
 
         int[] arr = new int[n+1];
-        arr[0] = 0;
         for(int i = 1; i<=n;i++){
             arr[i] = arr[i >> 1] + (i & 1);
         }
