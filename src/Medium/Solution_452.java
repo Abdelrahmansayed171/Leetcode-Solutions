ed586176ee9b50ca837419c9e81cab2aa160559b
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-12 16:00:02 +0200

    452 last medium intervals solved.

--- src/Medium/Solution_452.java
+++ src/Medium/Solution_452.java
@@ -1,8 +1,20 @@
 package Medium;
 
+import java.util.Arrays;
+
 public class Solution_452 {
     public int findMinArrowShots(int[][] points) {
-        int arrows = 0;
+        int arrows = 1;
+        Arrays.sort(points, (a,b) -> Integer.compare(a[0], b[0]));
+        int prevEnd = points[0][1];
+        for(int i = 1; i< points.length;i++){
+            if(points[i][0] <= prevEnd)
+                prevEnd = Math.min(prevEnd, points[i][1]);
+            else{
+                arrows++;
+                prevEnd = points[i][1];
+            }
+        }
         return arrows;
     }
 }
