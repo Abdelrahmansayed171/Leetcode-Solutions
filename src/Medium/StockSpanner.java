b5eddbb7dfada96a06a68e7b71b5a16d62a77362
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-12 18:19:36 +0200

    901 Last medium monotonic stack Solved. SHEET COMPLETED!!

--- src/Medium/StockSpanner.java
+++ src/Medium/StockSpanner.java
@@ -1,11 +1,20 @@
 package Medium;
 
-public class StockSpanner {
-    public StockSpanner() {
+import java.util.Stack;
 
+public class StockSpanner {
+    Stack<int[]> stack;
+    public StockSpanner() {
+        stack = new Stack<>();
     }
 
     public int next(int price) {
-
+        int res = 1;
+        while (!stack.isEmpty() && stack.peek()[0] <= price){
+            res += stack.peek()[1];
+            stack.pop();
+        }
+        stack.push(new int[] {price,res});
+        return res;
     }
 }
