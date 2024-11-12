5414f97e65ee150c9c86de8db66f536946280f7c
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-12 17:02:52 +0200

    739 first medium monotonic stack solved with cleaner code.

--- src/Medium/Solution_739.java
+++ src/Medium/Solution_739.java
@@ -7,17 +7,11 @@ public class Solution_739 {
         int n = temperatures.length;
         int[] res = new int[n];
         Stack<int[]> stack = new Stack<>();
-        stack.push(new int[]{temperatures[0], 0});
-        for(int i = 1; i < n;i++){
-            if(stack.peek()[0] < temperatures[i]){
-                while (!stack.isEmpty()){
-                    int idx = stack.peek()[1];
-                    int val = stack.peek()[0];
-                    if(val >= temperatures[i])
-                        break;
-                    res[idx] = i - idx;
-                    stack.pop();
-                }
+        for(int i = 0; i < n;i++){
+            while (!stack.isEmpty() && stack.peek()[0] < temperatures[i]){
+                int idx = stack.peek()[1];
+                res[idx] = i - idx;
+                stack.pop();
             }
             stack.push(new int[] {temperatures[i], i});
         }
