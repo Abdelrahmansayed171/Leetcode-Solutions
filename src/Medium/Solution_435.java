476feba814a726f0479b9c1d9410e50bcb7b763d
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-12 14:38:40 +0200

    435 first medium intervals Solved.

--- src/Medium/Solution_435.java
+++ src/Medium/Solution_435.java
@@ -1,8 +1,22 @@
 package Medium;
 
+import java.util.Arrays;
+
 public class Solution_435 {
     public int eraseOverlapIntervals(int[][] intervals) {
         int ans = 0;
+        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
+        int prevEnd = intervals[0][1];
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[i][0] >= prevEnd){
+                prevEnd = intervals[i][1];
+            }
+            else{
+                ans++;
+                prevEnd = Math.min(prevEnd,intervals[i][1]);
+            }
+        }
         return ans;
     }
 }
+
