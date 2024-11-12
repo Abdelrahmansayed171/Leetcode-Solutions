8120ae91987348948be89056afca296eded4108c
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-12 16:22:19 +0200

    739 first medium monotonic stack initialized.

---
+++ src/Medium/Solution_739.java
@@ -0,0 +1,10 @@
+package Medium;
+
+public class Solution_739 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+
+        return res;
+    }
+}
