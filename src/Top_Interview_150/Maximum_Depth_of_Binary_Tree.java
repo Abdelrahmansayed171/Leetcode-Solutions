4990b423a08886397c1c090ec2e67bef94d6f2ad
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-14 21:35:50 +0200

    Solved Easy DFS with 2 submissions

--- src/Top_Interview_150/Maximum_Depth_of_Binary_Tree.java
+++ src/Top_Interview_150/Maximum_Depth_of_Binary_Tree.java
@@ -4,6 +4,21 @@ import Medium.TreeNode;
 
 class Solution {
     public int maxDepth(TreeNode root) {
-        return 0;
+        return helper(root, 0);
     }
+
+    private int helper(TreeNode root, int depth) {
+        if(root == null)
+            return depth;
+        return Math.max(helper(root.left, depth+1), helper(root.right, depth+1));
+    }
+
+    /*
+    * Submission 2:
+    *   public int maxDepth(TreeNode root) {
+            if(root == null)
+                return 0;
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+    * */
 }
