c556fe19db10844bbb24077418c91263585f91b6
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-17 18:17:59 +0200

    Symmetric (mirror) Tree 4th Easy DFS Problem Solved Successfully.

--- src/Top_Interview_150/Symmetric_Tree.java
+++ src/Top_Interview_150/Symmetric_Tree.java
@@ -4,6 +4,13 @@ import Medium.TreeNode;
 
 public class Symmetric_Tree {
     public boolean isSymmetric(TreeNode root) {
-        return false;
+        return solve(root.left,root.right);
+    }
+    public boolean solve(TreeNode r1, TreeNode r2){
+        if(r1 == null && r2 == null)
+            return true;
+        if(r1 == null || r2 == null || r1.val != r2.val)
+            return false;
+        return solve(r1.right, r2.left) && solve(r1.left,r2.right);
     }
 }
