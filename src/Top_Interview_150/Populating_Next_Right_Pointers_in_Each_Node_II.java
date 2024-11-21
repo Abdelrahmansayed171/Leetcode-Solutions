c837d5acd2e2ad58acdee4de6d6a8f115e815287
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-21 12:14:17 +0200

    Medium 7th DFS Problem Solved with shorter code TOP 150.

--- src/Top_Interview_150/Populating_Next_Right_Pointers_in_Each_Node_II.java
+++ src/Top_Interview_150/Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -9,31 +9,20 @@ import java.util.Queue;
 public class Populating_Next_Right_Pointers_in_Each_Node_II {
     public Node connect(Node root) {
         if (root == null) return null;
-
         Queue<Node> bfs = new LinkedList<>();
         bfs.add(root);
-
         while (!bfs.isEmpty()) {
             int n = bfs.size();
             Node prev = null;
-
             for (int i = 0; i < n; i++) {
                 Node curr = bfs.poll();
-                if (prev != null) {
+                if (prev != null)
                     prev.next = curr;
-                }
                 prev = curr;
-
-                if (curr.left != null) {
+                if (curr.left != null)
                     bfs.add(curr.left);
-                }
-                if (curr.right != null) {
+                if (curr.right != null)
                     bfs.add(curr.right);
-                }
-            }
-
-            if (prev != null) {
-                prev.next = null;
             }
         }
         return root;
