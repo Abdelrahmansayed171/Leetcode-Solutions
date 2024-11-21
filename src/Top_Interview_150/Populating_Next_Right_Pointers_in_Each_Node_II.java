64d82f284767efc7fc0803674091c312b44c20e5
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-21 09:24:13 +0200

    Medium 7th DFS Problem reqs Initialized TOP 150.

---
+++ src/Top_Interview_150/Populating_Next_Right_Pointers_in_Each_Node_II.java
@@ -0,0 +1,10 @@
+package Top_Interview_150;
+
+// Medium
+// 117. Populating Next Right Pointers in Each Node II
+
+public class Populating_Next_Right_Pointers_in_Each_Node_II {
+    public Node connect(Node root) {
+        return null;
+    }
+}
