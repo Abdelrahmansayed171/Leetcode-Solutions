cb84c52a2d172f262f15ca662d28616958fbb4b7
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-21 12:17:05 +0200

    Medium 8th DFS Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Flatten_Binary_Tree_to_Linked_List.java
@@ -0,0 +1,12 @@
+package Top_Interview_150;
+
+
+import Medium.TreeNode;
+
+// Medium
+// 114. Flatten Binary Tree to Linked List
+public class Flatten_Binary_Tree_to_Linked_List {
+    public void flatten(TreeNode root) {
+
+    }
+}
