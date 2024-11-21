384c95ec59e5dcfdfdf55e13d34011ca7724a398
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-21 12:51:56 +0200

    Medium 8th DFS Problem Solved TOP 150.

--- src/Top_Interview_150/Flatten_Binary_Tree_to_Linked_List.java
+++ src/Top_Interview_150/Flatten_Binary_Tree_to_Linked_List.java
@@ -7,6 +7,19 @@ import Medium.TreeNode;
 // 114. Flatten Binary Tree to Linked List
 public class Flatten_Binary_Tree_to_Linked_List {
     public void flatten(TreeNode root) {
+        if(root == null)
+            return;
+        if(root.left != null) {
+            rightMost(root.left).right = root.right;
+            root.right = root.left;
+        }
+        root.left = null;
+        flatten(root.right);
+    }
 
+    private TreeNode rightMost(TreeNode root){
+        if(root.right == null)
+            return root;
+        return rightMost(root.right);
     }
 }
