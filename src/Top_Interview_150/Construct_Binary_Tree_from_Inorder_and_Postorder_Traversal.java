e68599153e86341f8450c87f9d58184120e2e4dd
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-21 07:22:31 +0200

    Medium 6th DFS Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.java
@@ -0,0 +1,9 @@
+package Top_Interview_150;
+
+import Medium.TreeNode;
+
+public class Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        return null;
+    }
+}
