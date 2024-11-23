ff543140acf1ac28625a31251daf24030f8b6abd
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-23 19:11:58 +0200

    ##Bad## Easy 9th DFS Problem Solved TOP 150.

--- src/Top_Interview_150/Path_Sum.java
+++ src/Top_Interview_150/Path_Sum.java
@@ -2,8 +2,15 @@ package Top_Interview_150;
 
 import Medium.TreeNode;
 
+// Easy
+// 112. Path Sum
 public class Path_Sum {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return false;
+        if(root == null)
+            return false;
+
+        if(root.left == null && root.right == null)
+            return root.val == targetSum;
+        return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right, targetSum-root.val);
     }
 }
