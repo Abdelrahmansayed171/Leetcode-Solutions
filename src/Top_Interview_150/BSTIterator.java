62c4f12164c78e688eaf4f20022846315823da58
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-24 10:59:20 +0200

    Medium 11th DFS Problem Solved TOP 150.

--- src/Top_Interview_150/BSTIterator.java
+++ src/Top_Interview_150/BSTIterator.java
@@ -2,18 +2,31 @@ package Top_Interview_150;
 
 import Medium.TreeNode;
 
+import java.util.Stack;
+
 // Medium
 // 173. Binary Search Tree Iterator
 public class BSTIterator {
+    Stack<TreeNode> inOrder;
     public BSTIterator(TreeNode root) {
+        inOrder = new Stack<>();
+        leftMost(root);
+    }
 
+    private void leftMost(TreeNode root) {
+        while (root != null){
+            inOrder.push(root);
+            root = root.left;
+        }
     }
 
     public int next() {
-
+        TreeNode res = inOrder.pop();
+        leftMost(res.right);
+        return res.val;
     }
 
     public boolean hasNext() {
-
+        return !inOrder.isEmpty();
     }
 }
