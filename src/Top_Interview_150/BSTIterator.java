b1a57b0933ee49c15750bd709cd9507090a82e72
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-24 09:27:16 +0200

    Medium 11th DFS Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/BSTIterator.java
@@ -0,0 +1,19 @@
+package Top_Interview_150;
+
+import Medium.TreeNode;
+
+// Medium
+// 173. Binary Search Tree Iterator
+public class BSTIterator {
+    public BSTIterator(TreeNode root) {
+
+    }
+
+    public int next() {
+
+    }
+
+    public boolean hasNext() {
+
+    }
+}
