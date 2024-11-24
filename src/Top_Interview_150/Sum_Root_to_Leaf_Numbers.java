f87b345b67d32cc9a32788fca22cba20feb1397b
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-24 09:25:09 +0200

    Medium 10th DFS Problem Solved TOP 150.

--- src/Top_Interview_150/Sum_Root_to_Leaf_Numbers.java
+++ src/Top_Interview_150/Sum_Root_to_Leaf_Numbers.java
@@ -1,9 +1,21 @@
 package Top_Interview_150;
 
+import Medium.TreeNode;
+
 // Medium
 // 129. Sum Root to Leaf Numbers
 public class Sum_Root_to_Leaf_Numbers {
     public int sumNumbers(TreeNode root) {
+        return solve(root,0);
+    }
 
+    private int solve(TreeNode root, int sum) {
+        if(root == null)
+            return 0;
+
+        sum+=root.val;
+        if(root.left == null && root.right == null)
+            return sum;
+        return solve(root.left, sum*10) + solve(root.right, sum*10);
     }
 }
