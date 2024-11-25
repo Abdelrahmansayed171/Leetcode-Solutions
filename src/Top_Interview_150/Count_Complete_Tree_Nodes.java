f0bff426655b4a62b49f102afddab7af413cd521
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-25 16:56:42 +0200

    Easy 12th DFS Problem Solved O(N) TOP 150.

--- src/Top_Interview_150/Count_Complete_Tree_Nodes.java
+++ src/Top_Interview_150/Count_Complete_Tree_Nodes.java
@@ -1,12 +1,25 @@
 package Top_Interview_150;
 
 import Medium.TreeNode;
+import com.sun.source.tree.Tree;
 
 // Easy
 // 222. Count Complete Tree Nodes
 
 public class Count_Complete_Tree_Nodes {
+    int nodes_num = 0;
     public int countNodes(TreeNode root) {
-        return 0;
+        if(root == null)
+            return 0;
+        count(root);
+        return nodes_num;
+    }
+
+    private void count(TreeNode root) {
+        if(root == null)
+            return;
+        nodes_num++;
+        countNodes(root.left);
+        countNodes(root.right);
     }
 }
