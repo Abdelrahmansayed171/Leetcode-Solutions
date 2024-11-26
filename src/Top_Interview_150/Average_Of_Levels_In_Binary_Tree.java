0160150ab3109b407f2aa1e2d531cd8fe54a6ce2
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-26 08:31:19 +0200

    E 15th BFS Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Average_Of_Levels_In_Binary_Tree.java
@@ -0,0 +1,14 @@
+package Top_Interview_150;
+
+import Medium.TreeNode;
+
+import java.util.List;
+
+// 637. Average of Levels in Binary Tree
+// Easy
+
+public class Average_Of_Levels_In_Binary_Tree {
+    public List<Double> averageOfLevels(TreeNode root) {
+
+    }
+}
