21a0bd4f82ec1e42eea4a6c40fd31fb8c420f0cf
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-26 07:27:39 +0200

    Medium 13th DFS Problem Initialized TOP 150 && LeetCode 75.

---
+++ src/Top_Interview_150/Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -0,0 +1,13 @@
+package Top_Interview_150;
+
+import Medium.TreeNode;
+
+// Medium
+// 236. Lowest Common Ancestor of a Binary Tree
+// common in leetcode75, leetcode150
+
+public class Lowest_Common_Ancestor_of_a_Binary_Tree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        return null;
+    }
+}
