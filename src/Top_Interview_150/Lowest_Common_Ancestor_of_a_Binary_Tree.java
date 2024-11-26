5fdc10690840b6e66f70a794279dc92092822479
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-26 07:57:11 +0200

    Medium 13th DFS Problem Solved TOP 150 && LeetCode 75.

--- src/Top_Interview_150/Lowest_Common_Ancestor_of_a_Binary_Tree.java
+++ src/Top_Interview_150/Lowest_Common_Ancestor_of_a_Binary_Tree.java
@@ -8,6 +8,16 @@ import Medium.TreeNode;
 
 public class Lowest_Common_Ancestor_of_a_Binary_Tree {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return null;
+        if(root == null || root == p || root == q)
+            return root;
+
+        TreeNode left = lowestCommonAncestor(root.left,p,q);
+        TreeNode right = lowestCommonAncestor(root.right,p,q);
+
+        if(left == null)
+            return right;
+        if(right==null)
+            return left;
+        return root;
     }
 }
