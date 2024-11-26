0ff9a6613e613099379f51556c2c1bd77575b2ea
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-11-26 08:29:03 +0200

    Medium 14th BFS & DFS Approaches Problem Solved TOP 150 && LeetCode 75.

--- src/Top_Interview_150/Binary_Tree_Right_Side_View.java
+++ src/Top_Interview_150/Binary_Tree_Right_Side_View.java
@@ -10,7 +10,49 @@ import java.util.Queue;
 // Medium
 // 199. Binary Tree Right Side View
 public class Binary_Tree_Right_Side_View {
-    public List<Integer> rightSideView(TreeNode root) {
 
+    //BFS Solution
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root == null)
+            return res;
+        queue.offer(root);
+        int n ;
+        while(!queue.isEmpty()){
+            n = queue.size();
+            for(int i =0;i < n;i++){
+                TreeNode cur = queue.poll();
+                if(i == n -1){
+                    res.add(cur.val);
+                }
+                if(cur.left != null)
+                    queue.offer(cur.left);
+                if(cur.right != null)
+                    queue.offer(cur.right);
+            }
+        }
+        return res;
     }
+
+
+    // DFS SOLUTION
+/*    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        helper(root, result, 0);
+        return result;
+    }
+
+    public void helper(TreeNode curr, List<Integer> result, int currDepth){
+        if(curr == null){
+            return;
+        }
+        if(currDepth == result.size()){
+            result.add(curr.val);
+        }
+
+        helper(curr.right, result, currDepth + 1);
+        helper(curr.left, result, currDepth + 1);
+
+    }*/
 }
