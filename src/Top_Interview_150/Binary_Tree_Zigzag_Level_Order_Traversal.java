1c68cb7ed937c188d6ef4db8737671cdaffb04e8
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-01 08:59:26 +0200

    Medium 17th BFS Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Binary_Tree_Zigzag_Level_Order_Traversal.java
@@ -0,0 +1,15 @@
+package Top_Interview_150;
+
+import Medium.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 103. Binary Tree Zigzag Level Order Traversal
+// Medium
+
+public class Binary_Tree_Zigzag_Level_Order_Traversal {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        return new ArrayList<>();
+    }
+}
