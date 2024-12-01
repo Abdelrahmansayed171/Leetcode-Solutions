41c3d84ad7cb0a181327a58d7c68c53266ff9099
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-01 08:35:53 +0200

    Medium 16th BFS Problem Solved TOP 150.

--- src/Top_Interview_150/Binary_Tree_Level_Order_Traversal.java
+++ src/Top_Interview_150/Binary_Tree_Level_Order_Traversal.java
@@ -2,14 +2,33 @@ package Top_Interview_150;
 
 import Medium.TreeNode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 // 102. Binary Tree Level Order Traversal
 // Medium
 
 public class Binary_Tree_Level_Order_Traversal {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        return new ArrayList<>();
+        if(root == null)
+            return new ArrayList<>();
+
+        Queue<TreeNode> bfs = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        bfs.offer(root);
+
+        while(!bfs.isEmpty()){
+            int n = bfs.size();
+            List<Integer> tmp = new ArrayList<>();
+            for(int i = 0;i < n;i++){
+                TreeNode curr = bfs.poll();
+                tmp.add(curr.val);
+                if(curr.left != null)
+                    bfs.offer(curr.left);
+                if(curr.right != null)
+                    bfs.offer(curr.right);
+            }
+            res.add(tmp);
+        }
+        return res;
     }
 }
