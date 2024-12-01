ac593bca68bde721d50e47beb80a5b52f2ad5ede
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-01 08:06:21 +0200

    Medium 16th BFS Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Binary_Tree_Level_Order_Traversal.java
@@ -0,0 +1,15 @@
+package Top_Interview_150;
+
+import Medium.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 102. Binary Tree Level Order Traversal
+// Medium
+
+public class Binary_Tree_Level_Order_Traversal {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        return new ArrayList<>();
+    }
+}
