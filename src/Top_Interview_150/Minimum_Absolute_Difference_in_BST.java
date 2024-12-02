358ff2767aff6dfe986b78ff150d9686f0c8e624
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-02 17:54:23 +0200

    Medium 18th BST Problem Solved TOP 150.

--- src/Top_Interview_150/Minimum_Absolute_Difference_in_BST.java
+++ src/Top_Interview_150/Minimum_Absolute_Difference_in_BST.java
@@ -11,6 +11,21 @@ import java.util.List;
 
 public class Minimum_Absolute_Difference_in_BST {
     public int getMinimumDifference(TreeNode root) {
+        List<Integer> inorderList = new ArrayList<>();
+        int res = Integer.MAX_VALUE;
+        inorderHelper(root, inorderList);
+        for(int i = 1; i < inorderList.size();i++){
+            res = Math.min(res, inorderList.get(i) - inorderList.get(i-1));
+        }
+        return res;
+    }
 
+    private void inorderHelper(TreeNode root, List<Integer> inorderList){
+        if(root == null)
+            return;
+
+        inorderHelper(root.left, inorderList);
+        inorderList.add(root.val);
+        inorderHelper(root.right, inorderList);
     }
 }
