d95c2fc695141580a63ab161452fb76e7b4e12e7
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-02 17:54:06 +0200

    Medium 18th BST Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Minimum_Absolute_Difference_in_BST.java
@@ -0,0 +1,16 @@
+package Top_Interview_150;
+
+import Medium.TreeNode;
+import com.sun.source.tree.Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 530. Minimum Absolute Difference in BST
+// Easy
+
+public class Minimum_Absolute_Difference_in_BST {
+    public int getMinimumDifference(TreeNode root) {
+
+    }
+}
