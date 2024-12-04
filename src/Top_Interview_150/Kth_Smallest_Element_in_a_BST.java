624270b84e43f359d661870dfff9260658a17687
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-04 16:04:21 +0200

    Medium 19th BST Problem Solved TOP 150.

--- src/Top_Interview_150/Kth_Smallest_Element_in_a_BST.java
+++ src/Top_Interview_150/Kth_Smallest_Element_in_a_BST.java
@@ -2,11 +2,24 @@ package Top_Interview_150;
 
 import Medium.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Medium
 // 230. Kth Smallest Element in a BST
 
 public class Kth_Smallest_Element_in_a_BST {
     public int kthSmallest(TreeNode root, int k) {
-        return 0;
+        List<Integer> inorderList = new ArrayList<>();
+        inorderHelper(root, inorderList);
+        return inorderList.get(k-1);
+    }
+    private void inorderHelper(TreeNode root, List<Integer> inorderList){
+        if(root == null)
+            return;
+
+        inorderHelper(root.left, inorderList);
+        inorderList.add(root.val);
+        inorderHelper(root.right, inorderList);
     }
 }
