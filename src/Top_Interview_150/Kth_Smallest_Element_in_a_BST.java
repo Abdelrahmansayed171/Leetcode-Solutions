da8e0dee43c8c84d310010b379a4dc2df36b28bb
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-04 15:57:17 +0200

    Medium 19th BST Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Kth_Smallest_Element_in_a_BST.java
@@ -0,0 +1,12 @@
+package Top_Interview_150;
+
+import Medium.TreeNode;
+
+// Medium
+// 230. Kth Smallest Element in a BST
+
+public class Kth_Smallest_Element_in_a_BST {
+    public int kthSmallest(TreeNode root, int k) {
+        return 0;
+    }
+}
