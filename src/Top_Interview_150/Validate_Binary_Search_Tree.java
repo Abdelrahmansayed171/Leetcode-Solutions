4a34cd4918c6d5989e1b24bfeeec7fb24616ac3e
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-04 16:59:08 +0200

    Medium 20th BST Problem Solved TOP 150.

--- src/Top_Interview_150/Validate_Binary_Search_Tree.java
+++ src/Top_Interview_150/Validate_Binary_Search_Tree.java
@@ -5,8 +5,25 @@ package Top_Interview_150;
 
 import Medium.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Validate_Binary_Search_Tree {
     public boolean isValidBST(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        for(int i = 1;i < list.size();i++){
+            if(list.get(i) <= list.get(i-1))
+                return false;
+        }
         return true;
+    }
+    public void inorder(TreeNode root, List<Integer> inorderlist){
+        if(root == null)
+            return;
+
+        inorder(root.left, inorderlist);
+        inorderlist.add(root.val);
+        inorder(root.right, inorderlist);
     }
 }
