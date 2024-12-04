4cef838d6162da24f6a15a49b4fb97b3f5523309
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-04 16:06:52 +0200

    Medium 20th BST Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Validate_Binary_Search_Tree.java
@@ -0,0 +1,12 @@
+package Top_Interview_150;
+
+// Medium
+// 98. Validate Binary Search Tree
+
+import Medium.TreeNode;
+
+public class Validate_Binary_Search_Tree {
+    public boolean isValidBST(TreeNode root) {
+        return true;
+    }
+}
