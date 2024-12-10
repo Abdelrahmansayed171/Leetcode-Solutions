e6193eec8e167e1abf80dca6281ce5ade8c5fc78
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-10 12:33:10 +0200

    Easy 21th 1D DP Problem solved TOP 150.

--- src/Top_Interview_150/Climbing_Stairs.java
+++ src/Top_Interview_150/Climbing_Stairs.java
@@ -3,8 +3,20 @@ package Top_Interview_150;
 // 70. Climbing Stairs
 // Easy
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Climbing_Stairs {
     public int climbStairs(int n) {
+        List<Integer> dp=  new ArrayList<>();
+        for(int i = 0; i <= n;i++){
+            if(i <= 3) {
+                dp.add(i);
+                continue;
+            }
+            dp.add(dp.get(dp.size()-1) + dp.get(dp.size()-2));
+        }
 
+        return dp.get(dp.size()-1);
     }
 }
