4aa1afcc34cce4279faca8f8aef2306d9098e039
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-12 16:04:38 +0200

    Medium 26th problem 5th Graph General Problem Solved TOP 150.

---
+++ src/Top_Interview_150/Course_Schedule.java
@@ -0,0 +1,10 @@
+package Top_Interview_150;
+
+// 207. Course Schedule
+// Medium
+
+public class Course_Schedule {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+    }
+}
