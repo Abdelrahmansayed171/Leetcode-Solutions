702319a6beb894f8206d5941e10c21bb0f2c60fa
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-12 12:52:29 +0200

    Medium 25th problem 4th Graph General Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Evaluate_Division.java
@@ -0,0 +1,11 @@
+package Top_Interview_150;
+import java.util.List;
+
+// 399. Evaluate Division
+// Medium
+
+public class Evaluate_Division {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+
+    }
+}
