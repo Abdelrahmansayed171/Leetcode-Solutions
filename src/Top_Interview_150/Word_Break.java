30a875a07395eefdd6819bd52b817f0990fba099
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-16 15:59:25 +0200

    Medium 29th problem 3rd 1D DP Problem Solved TOP 150.

---
+++ src/Top_Interview_150/Word_Break.java
@@ -0,0 +1,11 @@
+package Top_Interview_150;
+import java.util.*;
+
+// 139. Word Break
+// Medium
+
+public class Word_Break {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        return false;
+    }
+}
