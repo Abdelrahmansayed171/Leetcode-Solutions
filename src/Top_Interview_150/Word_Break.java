a8063e1467542e116597c907a1839e5acf2d351b
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-16 16:22:25 +0200

    (DP is Enhanced brute force)Medium 29th problem 3rd 1D DP Problem Solved TOP 150.

--- src/Top_Interview_150/Word_Break.java
+++ src/Top_Interview_150/Word_Break.java
@@ -6,6 +6,16 @@ import java.util.*;
 
 public class Word_Break {
     public boolean wordBreak(String s, List<String> wordDict) {
-        return false;
+        boolean[] dp = new boolean[s.length()+1];
+        dp[s.length()] = true;
+        for(int i =s.length()-1; i >= 0 ;i--){
+            for(String word : wordDict){
+                if(i + word.length() <= s.length() && s.startsWith(word,i))
+                    dp[i] = dp[i+word.length()];
+                if(dp[i])
+                    break;
+            }
+        }
+        return dp[0];
     }
 }
