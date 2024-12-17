ba0d44df8a65f064906530cf35930fd78f27190a
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-17 13:41:29 +0200

    Easy 33rd problem 2nd Array/String Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Remove_Element.java
@@ -0,0 +1,11 @@
+package Top_Interview_150;
+import java.util.*;
+
+// 27. Remove Element
+// Easy
+
+public class Remove_Element {
+    public int removeElement(int[] nums, int val) {
+        return 0;
+    }
+}
