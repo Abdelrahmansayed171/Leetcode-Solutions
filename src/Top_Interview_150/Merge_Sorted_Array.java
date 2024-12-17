0679978d1b3a2c912210b81a7fca6848f08cb206
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-17 11:56:19 +0200

    Easy 32nd problem 1st Array/String Problem Solved TOP 150.

--- src/Top_Interview_150/Merge_Sorted_Array.java
+++ src/Top_Interview_150/Merge_Sorted_Array.java
@@ -5,7 +5,31 @@ import java.util.*;
 //Easy
 
 public class Merge_Sorted_Array {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
 
+    // O(m+n) Algorithm
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = 0,j = 0, idx = 0;
+        int[] res = new int[m+n];
+        while(idx < m+n){
+            if(j >= n){
+                res[idx++] = nums1[i];
+                i++;
+            }
+            else if(i >= m){
+                res[idx++] = nums2[j];
+                j++;
+            }
+            else if(nums1[i] <= nums2[j]) {
+                res[idx++] = nums1[i];
+                i++;
+            }
+            else {
+                res[idx++] = nums2[j];
+                j++;
+            }
+        }
+        for(int i1 = 0; i1 < nums1.length;i1++){
+            nums1[i1] = res[i1];
+        }
     }
 }
