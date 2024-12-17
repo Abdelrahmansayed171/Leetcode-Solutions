18f74fa53721baf46162780c28a604d6f2c6cbd0
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-17 15:46:33 +0200

    Medium 34th problem 1st 2D DP Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Triangle.java
@@ -0,0 +1,11 @@
+package Top_Interview_150;
+import java.util.*;
+
+// 120. Triangle
+// Medium
+
+public class Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        return 0;
+    }
+}
