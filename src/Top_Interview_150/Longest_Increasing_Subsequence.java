83c82089e652c8108195cfbd62e6a665fad71985
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-17 10:08:13 +0200

    (Bottom-Up)Medium 31st problem 5th 1D DP Problem Solved TOP 150.

--- src/Top_Interview_150/Longest_Increasing_Subsequence.java
+++ src/Top_Interview_150/Longest_Increasing_Subsequence.java
@@ -6,6 +6,18 @@ import java.util.*;
 
 public class Longest_Increasing_Subsequence {
     public int lengthOfLIS(int[] nums) {
-
+        int[] dp = new int[nums.length];
+        dp[nums.length-1] = 1;
+        int res = -1;
+        for(int i = nums.length-1; i >= 0; i--){
+            dp[i] = 1;
+            for(int j = i + 1 ; j < nums.length;j++){
+                if(nums[i] < nums[j]){
+                    dp[i] = Math.max(dp[i], 1+dp[j]);
+                }
+            }
+            res = Math.max(res,dp[i]);
+        }
+        return res;
     }
 }
