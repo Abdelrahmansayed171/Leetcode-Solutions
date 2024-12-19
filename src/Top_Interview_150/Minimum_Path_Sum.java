fb95a502af2ea1bbca089ba6a464a1eacc676065
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-19 08:11:41 +0200

    Medium 35th problem 2nd 2D DP Problem Solved TOP 150.

--- src/Top_Interview_150/Minimum_Path_Sum.java
+++ src/Top_Interview_150/Minimum_Path_Sum.java
@@ -6,6 +6,15 @@ import java.util.*;
 
 public class Minimum_Path_Sum {
     public int minPathSum(int[][] grid) {
-        return 0;
+        int[] dp = new int[grid[0].length+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[dp.length-2] = 0;
+        for(int i = grid.length-1 ;i >= 0 ;i --){
+            for(int j = grid[0].length-1; j >= 0; j--){
+                grid[i][j] += Math.min(dp[j], dp[j+1]);
+                dp[j] = grid[i][j];
+            }
+        }
+        return grid[0][0];
     }
 }
