ef0d2c2b1d92750e69f7596ba572beb0d731fd16
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-19 09:17:15 +0200

    Medium 36th problem 3rd 2D DP Problem Solved TOP 150.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,13 @@
 import Easy.Solution_872;
 import Easy.TreeNode;
 import Medium.*;
+import Top_Interview_150.Unique_Paths_II;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        Solution_236 solution2352 = new Solution_236();
-//        solution2352.lowestCommonAncestor();
-//        TreeNode root1 = new TreeNode(
-//                1,
-//                new TreeNode(2),
-//                new TreeNode(200)
-//        );
-//        TreeNode root2 = new TreeNode(
-//                1,
-//                new TreeNode(2),
-//                new TreeNode(200)
-//        );
-//        System.out.println(solution2352.leafSimilar(root1, root2));
-/*        for (int i : solution2352.asteroidCollision(arr)) {
-            System.out.println(i+ " ");
-        }*/
+        Unique_Paths_II uniquePathsIi = new Unique_Paths_II();
+        System.out.println(uniquePathsIi.uniquePathsWithObstacles(new int[][] {}));
     }
 }
--- src/Top_Interview_150/Unique_Paths_II.java
+++ src/Top_Interview_150/Unique_Paths_II.java
@@ -6,6 +6,27 @@ import java.util.*;
 
 public class Unique_Paths_II {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        return 0;
+        if(obstacleGrid[0][0] == 1)
+            return 0;
+        int[] dp = new int[obstacleGrid[0].length+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[dp.length-2] = 1;
+        for(int i = obstacleGrid.length-1;i >=0; i--){
+            for(int j = obstacleGrid[0].length-1; j >= 0; j--){
+                if(obstacleGrid[i][j] == 1) {
+                    dp[j] = Integer.MAX_VALUE;
+                    continue;
+                }
+                if(dp[j] != Integer.MAX_VALUE) {
+                    obstacleGrid[i][j] += dp[j];
+                    dp[j] = obstacleGrid[i][j];
+                }
+                if(dp[j+1] != Integer.MAX_VALUE) {
+                    obstacleGrid[i][j] += dp[j + 1];
+                    dp[j] = obstacleGrid[i][j];
+                }
+            }
+        }
+        return obstacleGrid[0][0];
     }
 }
