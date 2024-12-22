a4aad75fbdfece275174f43e891cf488028a56b4
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-22 13:42:10 +0200

    Easy 39th problem 3rd Array/Strings Problem Solved with 2 pointers O(n) TOP 150.

--- src/Top_Interview_150/Remove_Duplicates_From_Sorted_Array.java
+++ src/Top_Interview_150/Remove_Duplicates_From_Sorted_Array.java
@@ -5,7 +5,20 @@ import java.util.*;
 // Easy
 
 public class Remove_Duplicates_From_Sorted_Array {
+    // O(n) - 2 Pointers
     public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for(int i = 1; i < nums.length;i++){
+            if(nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+
+    // O(nlog(n))
+    /*public int removeDuplicates(int[] nums) {
         int cnt = 0;
         for(int i = 0; i < nums.length - 1;i++){
             if(nums[i] == nums[i+1]) {
@@ -15,5 +28,5 @@ public class Remove_Duplicates_From_Sorted_Array {
         }
         Arrays.sort(nums);
         return nums.length - cnt;
-    }
+    }*/
 }
