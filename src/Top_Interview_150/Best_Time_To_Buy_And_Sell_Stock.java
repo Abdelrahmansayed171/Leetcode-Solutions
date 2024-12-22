b5ca37690a581f9f5c7ab45185eae38badd37daf
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-22 16:00:51 +0200

    Easy 41st problem 5th Array/Strings Problem Solved with Dynamic Programming TOP 150.

--- src/Top_Interview_150/Best_Time_To_Buy_And_Sell_Stock.java
+++ src/Top_Interview_150/Best_Time_To_Buy_And_Sell_Stock.java
@@ -6,6 +6,12 @@ import java.util.*;
 
 public class Best_Time_To_Buy_And_Sell_Stock {
     public int maxProfit(int[] prices) {
-        return 0;
+        int buy = prices[0] ;
+        int profit = 0;
+        for(int i = 1; i <prices.length;i++){
+            buy = Math.min(buy, prices[i]);
+            profit = Math.max(profit, prices[i] - buy);
+        }
+        return profit;
     }
 }
