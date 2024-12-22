9f705a1f00c8015614e42fb30fee5d52944dff35
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-22 15:37:30 +0200

    Easy 41st problem 5th Array/Strings Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Best_Time_To_Buy_And_Sell_Stock.java
@@ -0,0 +1,11 @@
+package Top_Interview_150;
+import java.util.*;
+
+// 121. Best Time to Buy and Sell Stock
+// Easy
+
+public class Best_Time_To_Buy_And_Sell_Stock {
+    public int maxProfit(int[] prices) {
+        return 0;
+    }
+}
