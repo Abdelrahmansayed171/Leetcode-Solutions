2ba4a8315470e29141c27dfcd266874924b31160
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2024-12-23 08:09:43 +0200

    Easy 44th problem 8th Array/Strings Problem Initialized TOP 150.

---
+++ src/Top_Interview_150/Longest_Common_Prefix.java
@@ -0,0 +1,11 @@
+package Top_Interview_150;
+import java.util.*;
+
+// 14. Longest Common Prefix
+// Easy
+
+public class Longest_Common_Prefix {
+    public String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+}
