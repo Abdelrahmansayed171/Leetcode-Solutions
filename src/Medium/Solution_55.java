4a10637c6531a361654cb39f069074afbe4c82da
Abdelrahman Sayed <abdelrahmansayed171@gmail.com>
2026-06-07 19:57:07 +0300

    1 med (Greedy) solved

---
+++ src/Medium/Solution_55.java
@@ -0,0 +1,13 @@
+package Medium;
+
+public class Solution_55 {
+    public boolean canJump(int[] nums) {
+        int maxJump = 0;
+        for(int i = 0; i < nums.length;i++){
+            if(i > maxJump)
+                return false;
+            maxJump = Math.max(maxJump, nums[i] + i);
+        }
+        return true;
+    }
+}
